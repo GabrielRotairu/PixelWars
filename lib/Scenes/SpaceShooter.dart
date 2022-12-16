e4776f3dc3f445f14046375e51ddef0f86500dc6
Gabriel Rotariu <dam.gabriel.rotariu@gmail.com>
2022-12-16 09:15:09 +0100

    Añadido del directorio Players y de la clase Player1 y añadido de la primera escena llamada SpaceShooter

--- lib/Scenes/SpaceShooter.dart
+++ lib/Scenes/SpaceShooter.dart
@@ -1,2 +1,11 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+
+
 class SpaceShooterGame extends FlameGame with HasTappables {
+
+
+
+
 }
